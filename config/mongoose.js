f203e4ef5a32595f877745193e56a2ca8a5c1835
Md Jibran <103884359+md1jibran@users.noreply.github.com>
2023-07-05 17:59:50 +0530

    Create mongoose.js

---
+++ config/mongoose.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/Polling_System');
+
+// Code for using the cloud mongodb atlas 
+const connectParams={
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}
+
+// Code for manually using the mongodb of local system
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'error connecting to database'));
+
+db.once('open', ()=>{
+    console.log("successfully connected to database : mongoDB");
+});
+
+module.exports = mongoose;
